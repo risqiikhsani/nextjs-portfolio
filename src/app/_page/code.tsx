58a31191369f091661c9237b1a362cc0edcd7a31
risqi2 <risqiikhsani12@gmail.com>
2023-08-17 20:45:04 +0700

    added typing animation

---
+++ src/app/_page/code.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { Typography } from "@mui/material";
+import { TypeAnimation } from "react-type-animation";
+
+
+    
+
+
+
+ 
+    
+
+
+export default function Code() {
+  return (
+    <>
+    <Typography>
+      <TypeAnimation
+        style={{ whiteSpace: "pre-line", display: "block" }}
+        sequence={[
+        `const his_tech_passion = [\n"🚀 Full-Stack Developer",\n"🎨 Front-End Enthusiast",\n"🕹️ Game Development Enthusiast"\n"⚙️ Back-End Wizard",\n"🛠️ DevOps Magician",\n];\n
+
+        const his_other_passion = [\n"🐱 Cat Lover Extraordinaire 🐾",\n"🏞️ Nature Explorer and Enthusiast 🌳"\n];`,
+          1000,
+          "",
+        ]}
+        deletionSpeed={99}
+        repeat={Infinity}
+      />
+    </Typography>
+    </>
+  );
+}
